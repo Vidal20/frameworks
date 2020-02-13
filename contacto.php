fd0c52740705cf42021a2d7a740f0e4930d2a869
Vidal20 <sebastianvidiaz@gmail.com>
2020-02-12 19:29:26 -0600

    Diseño de contacto.php fase inicial

--- contacto.php
+++ contacto.php
@@ -19,7 +19,10 @@ include("Logueo/nav.php")
 include("Logueo/login.php")
 ?>
 <body>
-	<img  class="about" src="resources/QUIENESSOMOS.JPG"/>
+	<!--
+	<img  class="about" src="resources/QUIENESSOMOS.JPG"/>-->
+	<div class="box-contactanos">Contactanos</div>
+	
 </body>
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
