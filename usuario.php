554d1393d606086fe922daf2817fde2e72a77e12
Vidal20 <sebastianvidiaz@gmail.com>
2020-02-13 14:40:22 -0600

    Correciones en errores por conflicto de archivos

--- usuario.php
+++ usuario.php
@@ -31,17 +31,6 @@ include("barranav.php")
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-<<<<<<< HEAD
-	<div class="productos">
-
-		<ul class="lista-producto">
-			<li>
-				<div class="containerimg">
-					<img src="resources/img1.jpg" alt="" class="imgproducto">
-						<div class="informacion">
-							<p class="nombreimg">Ventanas</p>
-							<a href="" class="informacion">Ver mas información</a>
-=======
 	<div class="container-user">
 
 		<ul class="list-image-user">
@@ -51,54 +40,30 @@ include("barranav.php")
 						<div class="informacion">
 							<p class="nombre_img">Ventanas</p>
 							<a href="" class="view_informacion">Ver mas información</a>
->>>>>>> 0f97941723dde8b9907db081846bc9f910e6a074
 						</div>
 				</div>
 			</li>
 
 			<li>
-<<<<<<< HEAD
-				<div class="containerimg">
-					<img src="resources/img2.jpg" alt="" class="imgproducto">
-						<div class="informacion">
-							<p class="nombreimg">Ventanas</p>
-							<a href="" class="informacion">Ver mas información</a>
-=======
 				<div class="container__img">
 					<img src="resources/ventana-14347_14347.jpg" alt="" class="img__">
 						<div class="informacion">
 							<p class="nombre_img">Ventanas</p>
 							<a href="" class="view_informacion">Ver mas información</a>
->>>>>>> 0f97941723dde8b9907db081846bc9f910e6a074
 						</div>
 				</div>
 			</li>
 
 			<li>
-<<<<<<< HEAD
-				<div class="containerimg">
-					<img src="resources/img3.jpg" alt="" class="imgproducto">
-						<div class="informacion">
-							<p class="nombreimg">Ventanas</p>
-							<a href="" class="informacion">Ver mas información</a>
-=======
 				<div class="container__img">
 					<img src="resources/Puertas-de-aluminio-10.jpg" alt="" class="img__">
 						<div class="informacion">
 							<p class="nombre_img">Ventanas</p>
 							<a href="" class="view_informacion">Ver mas información</a>
->>>>>>> 0f97941723dde8b9907db081846bc9f910e6a074
 						</div>
 				</div>
 			</li>
 		</ul>
-<<<<<<< HEAD
-	</div>
-
-
-
-
-=======
 
 		<ul class="list-image-user">
 			<li>
@@ -164,7 +129,6 @@ include("barranav.php")
 			</li>
 		</ul>
 	</div>
->>>>>>> 0f97941723dde8b9907db081846bc9f910e6a074
 
 
 
