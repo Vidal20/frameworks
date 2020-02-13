74e0ce8da9b4a3ef9294f505fd2d37ae24816cc5
Vidal20 <sebastianvidiaz@gmail.com>
2020-02-13 14:20:29 -0600

    Correciones

--- usuario.php
+++ usuario.php
@@ -26,4 +26,46 @@ echo "<title>$usuario</title>";
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+	<div class="productos">
+
+		<ul class="lista-producto">
+			<li>
+				<div class="containerimg">
+					<img src="resources/img1.jpg" alt="" class="imgproducto">
+						<div class="informacion">
+							<p class="nombreimg">Ventanas</p>
+							<a href="" class="informacion">Ver mas información</a>
+						</div>
+				</div>
+			</li>
+
+			<li>
+				<div class="containerimg">
+					<img src="resources/img2.jpg" alt="" class="imgproducto">
+						<div class="informacion">
+							<p class="nombreimg">Ventanas</p>
+							<a href="" class="informacion">Ver mas información</a>
+						</div>
+				</div>
+			</li>
+
+			<li>
+				<div class="containerimg">
+					<img src="resources/img3.jpg" alt="" class="imgproducto">
+						<div class="informacion">
+							<p class="nombreimg">Ventanas</p>
+							<a href="" class="informacion">Ver mas información</a>
+						</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+
+
+
+
+
+
+
 </html>
